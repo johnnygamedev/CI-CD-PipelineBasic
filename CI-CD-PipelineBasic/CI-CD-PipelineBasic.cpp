125513c6ff1394bab9f0e05180666e3e24dcb2fa
johnny macmillan <jpmac67@icloud.com>
2026-08-20 20:48:02 +0100

    small

--- CI-CD-PipelineBasic/CI-CD-PipelineBasic.cpp
+++ CI-CD-PipelineBasic/CI-CD-PipelineBasic.cpp
@@ -7,7 +7,8 @@
  
 int main()
 {
-    std::cout << "kaboom" << std::endl;
+    std::cout << "kaboom" << std::endl; 
+    std::cout << "Keep the git hub green - caus i worked at 6am and i am tired..." << std::endl;
 }
 
 //new 
